46c6a4c94ec57b7424e24544d9074ced7fc2605e
echozyr2001 <echo.zyr.2001@gmail.com>
2024-03-22 16:39:40 +0800

    refactor: build footer for marketing page
    add some contries' flag in the footer of marketing page

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,61 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-center h-full space-x-2">
+        <Button size="lg" className="w-full" variant="ghost">
+          <Image
+            src="/hr.svg"
+            alt="Croatian"
+            height={50}
+            width={50}
+            className="mr-4 rounded-md"
+          />
+          Croatian
+        </Button>
+        <Button size="lg" className="w-full" variant="ghost">
+          <Image
+            src="/fr.svg"
+            alt="French"
+            height={50}
+            width={50}
+            className="mr-4 rounded-md"
+          />
+          French
+        </Button>
+        <Button size="lg" className="w-full" variant="ghost">
+          <Image
+            src="/es.svg"
+            alt="Spanish"
+            height={50}
+            width={50}
+            className="mr-4 rounded-md"
+          />
+          Spanish
+        </Button>
+        <Button size="lg" className="w-full" variant="ghost">
+          <Image
+            src="/it.svg"
+            alt="Italian"
+            height={50}
+            width={50}
+            className="mr-4 rounded-md"
+          />
+          Italian
+        </Button>
+        <Button size="lg" className="w-full" variant="ghost">
+          <Image
+            src="/jp.svg"
+            alt="Japanese"
+            height={50}
+            width={50}
+            className="mr-4 rounded-md"
+          />
+          Japanese
+        </Button>
+      </div>
     </footer>
   );
 };
