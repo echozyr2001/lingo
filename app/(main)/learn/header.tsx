accb343c06576d04eee455f294405a50a594df1d
echozyr2001 <echo.zyr.2001@gmail.com>
2024-03-27 11:55:51 +0800

    fix: fix wrong mt
    fix wrong mt

--- app/(main)/learn/header.tsx
+++ app/(main)/learn/header.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Header = ({ title }: Props) => {
   return (
-    <div className="sticky top-0 bg-white pb-3 flex items-center justify-between border-b-2 mb-5 text-neutral-400 lg:pt-[28px]">
+    <div className="sticky top-0 bg-white pb-3 flex items-center justify-between border-b-2 mb-5 text-neutral-400 lg:pt-[28px] lg:mt-[-28px]">
       <Link href="/courses">
         <Button variant="ghost" size="sm">
           <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400" />
